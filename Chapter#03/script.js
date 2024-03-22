52fb97c20e2420257c74e15e7d6de623276a015f
AbdulManafAbdullah <mannanzuni91@gmail.com>
2024-03-22 05:38:27 +0500

    assignments

---
+++ Chapter#03/script.js
@@ -0,0 +1,18 @@
+
+var age = 'I am 33 years old'
+
+alert(age)
+
+var birthyear = 'My birth year is 1991'
+
+alert(birthyear)
+
+
+
+var name = 'JOHN'
+
+var  titlle = ' cotton_clothes'
+
+var quantity =  10
+
+alert(name  + titlle + quantity)
---
+++ Chapter#05/script.js
@@ -0,0 +1,263 @@
+
+document.write(`<h1>1st and 2nd task</h1>`)
+
+
+var num1 = 3
+var num2 = 5
+var sum = num1 + num2
+
+
+
+
+
+
+document.write( sum)
+
+
+
+
+var num = 3
+var numm = 5
+var divide = num1 / num2
+
+
+document.write(divide)
+
+
+
+var num = 3
+var numm = 5
+var multiply = num1 * num2
+
+
+document.write( multiply)
+
+
+
+var num1 = 3
+var num2 = 5
+var minus = num1 - num2
+
+
+document.write(minus)
+
+
+
+
+document.write(`<h1>3rd task</h1>`)
+
+
+
+
+var nom ;
+
+
+
+document.write(`<h3>Value after variable declaration is ${nom} </h3> `)
+
+
+nom = 5
+
+document.write(`<h3>initial value : ${nom} </h3> `)
+
+
+
+
+document.write(`<h3>value after increment is</h3>`)
+
+
+var nom = 5;
+var nome = ++nom
+
+document.write(nom)
+document.write(nome)
+
+var nom = 6 + 7
+
+document.write(`<h3>value after addition is :  ${nom} </h3> `)
+
+
+var nom = 13
+var nomy = --nom
+
+document.write(`<h3>Value after decremenation is : ${nom} ${nomy}</h3> `)
+
+
+var remainder = nom / 3
+
+document.write(`<h3>output is : ${remainder}</h3> `)
+
+
+document.write(`<h1>4th task</h1>`)
+
+var ticket_price = 600
+
+var cost = 600 * 5
+
+document.write(`<h3>Total cost to buy five tickets for a movie is <i>${cost}</i></h3>`)
+
+
+
+document.write(`<h1>5th task</h1>`)
+
+
+var userinput = prompt("enter any number of any table")
+
+document.write(`<h4>${userinput} x 1 = ${userinput*1}</h4>`)
+document.write(`<h4>${userinput} x 2 = ${userinput*2}</h4>`)
+document.write(`<h4>${userinput} x 3 = ${userinput*3}</h4>`)
+document.write(`<h4>${userinput} x 4 = ${userinput*4}</h4>`)
+document.write(`<h4>${userinput} x 5 = ${userinput*5}</h4>`)
+document.write(`<h4>${userinput} x 6 = ${userinput*6}</h4>`)
+document.write(`<h4>${userinput} x 7 = ${userinput*7}</h4>`)
+document.write(`<h4>${userinput} x 8 = ${userinput*8}</h4>`)
+document.write(`<h4>${userinput} x 9 = ${userinput*9}</h4>`)
+document.write(`<h4>${userinput} x 10 = ${userinput*10}</h4>`)
+
+
+
+document.write(`<h1>6th task</h1>`)
+
+
+// Store a Celsius temperature into a variable
+var celsiusTemp = 25;
+
+// Convert Celsius to Fahrenheit
+var fahrenheitTemp = (celsiusTemp * 9/5) + 32;
+
+// Output the result
+document.write(celsiusTemp + "°C is " + fahrenheitTemp + "°F");
+
+// Store a Fahrenheit temperature into a variable
+var fahrenheitTemp2 = 77;
+
+// Convert Fahrenheit to Celsius
+var celsiusTemp2 = (fahrenheitTemp2 - 32) * 5/9;
+
+// Output the result
+document.write(fahrenheitTemp2 + "°F is " + celsiusTemp2 + "°C");
+
+
+
+
+
+
+document.write(`<h1>7th task</h1>`)
+
+
+
+
+document.write(`<h4>Price of item 1 is 650</h4>`)
+document.write(`<h4>quantity of item 1 is 3</h4>`)
+document.write(`<h4>price of item 2 is 100</h4>`)
+document.write(`<h4>quantity of item 2 is 7</h4>`)
+document.write(`<h4>shipping charges 100</h4>`)
+
+document.write(`<h4>total course of your order is</h4>`)
+
+
+
+
+var itemOnePrice = 650
+var itemTwoPrice = 100
+
+var quantityOrderedItemsOne = 3
+var quantityOrderedItemsTwo = 7
+
+var shippingCharges = 100
+
+var totalCostOfOne = itemOnePrice * quantityOrderedItemsOne
+var totalCostOfTwo = itemTwoPrice * quantityOrderedItemsTwo
+var coster = totalCostOfOne + totalCostOfTwo + shippingCharges
+
+
+document.write(coster)
+
+
+
+document.write(`<h1>8th task</h1>`)
+
+
+document.write(`<h4>total marks:980</h4>`)
+document.write(`<h4>obtain marks:804</h4>`)
+
+
+
+var totalMarks = 980
+var obtainMarks = 804
+
+percentage =   obtainMarks / totalMarks
+
+document.write(`<h4>Percentage ${percentage}</h4>`)
+
+
+
+document.write(`<h1>9th task</h1>`)
+
+
+var usDollars = 10
+var saudiRiyals = 25
+
+
+var convertUsdToPkr = 104.80
+var convertSaudiRiyalToPkr = 28
+
+
+var conversion = (usDollars * convertUsdToPkr) + (saudiRiyals * convertSaudiRiyalToPkr)
+
+document.write(`<h4>Amount after conversion in Pakistani Rupees is ${conversion}</h4>`)
+
+
+
+
+document.write(`<h1>10th task</h1>`)
+
+var valueOfNum = 10
+
+var arithmaticOperations = valueOfNum  + 5 * 10 / 2
+
+document.write(arithmaticOperations)
+
+
+
+document.write(`<h1>Task No 11</h1>`)
+
+
+var currentYear = 2024
+var birthYear = 2008
+
+document.write(`<h5>Your current age is ${currentYear}</h5>`)
+document.write(`<h5>Your birth year is ${birthYear}</h5>`)
+
+
+var calculate = currentYear - birthYear
+
+document.write(`<h5>Your exact age is ${calculate}</h5>`)
+
+
+
+
+// document.write(`<h1>Task No 12</h1>`)
+
+// var favSnack = 'Lays'
+//  var myOriginalAge = 15
+//  var maxAge = 65
+//  var estimatedAmountPerDay = 3
+
+//  var totalAge = maxAge - myOriginalAge
+// //  var totalDaysRemaining = totalAge * 365
+
+//  var finalResult = totalAge * 365 * estimatedAmountPerDay
+//  console.log(finalResult )
+
+document.write(`<h1>taskno 12</h1>`)
+
+var radius = 20
+
+var circumferrence = 2 * 3.142 * radius
+
+var area = 3.142 * radius * radius
+
+document.write(`<h4>Radius is: ${radius}</h4>`)
+document.write(`<h4>Circumferrence is: ${circumferrence}</h4>`)
+document.write(`<h4>Area is: ${area}</h4>`)
